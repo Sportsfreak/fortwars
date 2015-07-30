80e50873186102cf840bf3ca06649fc71f5cab38
crazyscouter <crazyscouter1@gmail.com>
2015-07-30 00:33:02 -0700

    Default class

---
+++ gamemode/classes.lua
@@ -0,0 +1,14 @@
+fortwars:registerClass("Default", {
+	health = 100,
+	armor  = 0,
+	damage = 0, //modifier. will be used to multiply. so to do 150 damage do 1.5
+	speed  = nil //leave nil for default
+	energy  = 100,
+	regen  = 1, //variable per second
+	jump   = nil,
+	model  = 'path/to/model.mdl',
+	loadout = {} //leave blank for default gm loadout
+	ability = function(ply) //this is called in a think hook. basically, check before you do, here.
+		return
+	end
+})
